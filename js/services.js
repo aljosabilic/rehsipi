27e928e525b486b184aecb9a5ef4000c99a403f3
Aljosa Bilic <aljosa.bilic@gmail.com>
2014-10-11 18:23:02 +0200

    Progress

--- js/services.js
+++ js/services.js
@@ -1,9 +1,6 @@
 angular.module('rehsipi.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('RecipeService', function($http) {
+.factory('RecipeService', function($http, $location) {
   // Might use a resource here that returns a JSON array
   var recipes = { ean_code: '', list: [] };
 
@@ -12,17 +9,19 @@ angular.module('rehsipi.services', [])
         if (recipes['ean_code'] == ean_code) {
             // Pass
         } else {
-            $http.get('http://hackzurich14.herokuapp.com/api/' + ean_code).then(function (resp) {
-                console.log(resp);
+            recipes['list'] = [];
 
-                recipes['ean_code'] = ean_code
-                var product_name = resp.data['product']['name_english']
+            $http.get('http://hackzurich14.herokuapp.com/api/' + ean_code).then(function (resp) {
+                recipes['ean_code'] = ean_code;
+                var product_name = resp.data['product']['name_english'];
 
                 recipes['list'].push.apply(recipes['list'], [
                     { id: 0, title: product_name, description: 'A product', img: 'http://placehold.it/100x100'}
                 ]);
             }, function (err) {
                 console.error(err);
+                alert('Product not found');
+                $location.path('/app/start');
             });
         }
 
